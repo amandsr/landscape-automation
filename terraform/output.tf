b013e17aefa6329c94c1cabe5592fa68e49ef4cb
amandsr <amandsr@users.noreply.github.com>
2025-07-08 14:56:22 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,8 @@
 output "public_ip" {
   value = aws_instance.landscape.public_ip
 }
+
+output "ec2_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.landscape.public_dns
+}
